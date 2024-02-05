c62de97afbdab10ddeef36e80b178c0b5f293386
Atharv Pathak <149372274+pathakjiop@users.noreply.github.com>
2024-02-05 09:52:30 +0530

    Herb Details showcase

---
+++ AIS/Front-End/src/components/HerbDetails.js
@@ -0,0 +1,31 @@
+// HerbDetails.js
+import React from 'react';
+import styles from './HerbDetails.module.css'
+
+const HerbDetails = ({ herbData }) => {
+  return (
+    <div className={styles.herbDetails}>
+      <h2>Herb Name: {herbData['Herb Name']}</h2>
+      <h3>Benefits:</h3>
+      <ul>
+        {herbData.Benefits.map((benefit, index) => (
+          <li key={index}>{benefit}</li>
+        ))}
+      </ul>
+      <h3>Properties:</h3>
+      <ul>
+        {herbData.Properties.map((property, index) => (
+          <li key={index}>{property}</li>
+        ))}
+      </ul>
+      <h3>Precautions:</h3>
+      <ul>
+        {herbData.Precautions.map((precaution, index) => (
+          <li key={index}>{precaution}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default HerbDetails;
