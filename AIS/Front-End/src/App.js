6fae7334c9ff44893924afbbfc288599e505c609
Atharv Pathak <149372274+pathakjiop@users.noreply.github.com>
2024-02-05 09:48:26 +0530

    Herbs Details Frontend

--- AIS/Front-End/src/App.js
+++ AIS/Front-End/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import SearchForm from './components/SearchForm';
+import HerbDetails from './components/HerbDetails';
+import { SpinnerCircular } from 'spinners-react';
 import './App.css';
+import { Nav, Tab, TabContainer, TabContent } from 'react-bootstrap';
 
 function App() {
+  const [herbData, setHerbData] = useState(null);
+  const [remedyData, setRemedyData] = useState(null);
+  const [activeTab, setActiveTab] = useState('link-1');
+
+  const [loading, setLoading] = useState(false);
+
+  const handleSetHerbData = (data) => {
+    setHerbData(data);
+    setLoading(false);
+  };
+
+  const handleSetRemedyData = (data) => {
+    setRemedyData(data);
+    setLoading(false);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ayurvedic Information System</h1>
+
+      <Nav fill variant="tabs" activeKey={activeTab} onSelect={(selectedKey) => setActiveTab(selectedKey)}>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Herb Details</Nav.Link>
+        </Nav.Item>
+          
+      </Nav>
+      
+      <Tab.Content>
+        <Tab.Container activeKey={activeTab}>
+          <Tab.Content>
+            <Tab.Pane eventKey="link-1">
+            <h2>Herb Information</h2>
+              <SearchForm setHerbData={handleSetHerbData} setLoading={setLoading} />
+              {loading && <SpinnerCircular color='#000000'/>}
+              {herbData && <HerbDetails herbData={herbData} />}
+            </Tab.Pane>
+          </Tab.Content>
+        </Tab.Container>
+      </Tab.Content>
     </div>
   );
 }
