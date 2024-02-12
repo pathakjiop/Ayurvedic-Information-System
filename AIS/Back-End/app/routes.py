b98799120d53285bbb9357b6382bff8c156245ee
Atharv Pathak <149372274+pathakjiop@users.noreply.github.com>
2024-02-12 09:31:34 +0530

    Added Remedy Details

--- AIS/Back-End/app/routes.py
+++ AIS/Back-End/app/routes.py
@@ -15,7 +15,6 @@ def get_herb_details():
     data = {
       "contents": [{
         "parts":[{
-          # herbs prompt
           "text": f"{data.get("herb_name")} herb its properties in JSON. Give columns as Herb Name, Benefits, Properties, Precautions"}]}]}
     
     response = requests.post(url, json=data)
@@ -30,3 +29,27 @@ def get_herb_details():
     else:
         return "No text found in the response."
     
+@app.route('/get-remedy-details', methods=['POST'])
+def get_remedy_details():
+    # Assuming JSON data is sent in the request body
+    data = request.json
+    print("AILMENT DETAILS: ", data.get('ailment_name'))
+    # Process the data and return a response
+    # Example response
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key={os.environ.get('GEMINI_API_KEY')}"
+    data = {
+      "contents": [{
+        "parts":[{
+          "text": f"Act as 10 years experience expert of ayurveda and tell me the ayurvedic herbs remedy when {data.get("ailment_name")} in JSON with proper in sequence columns of Diagnosis, What to eat, When to eat, How to prepare it."}]}]}
+    
+    response = requests.post(url, json=data)
+
+    parsed_response = response.json()
+
+    if "candidates" in parsed_response and len(parsed_response["candidates"]) > 0:
+        candidate = parsed_response["candidates"][0]
+        if "content" in candidate and "parts" in candidate["content"] and len(candidate["content"]["parts"]) > 0:
+            text = candidate["content"]["parts"][0]["text"]
+            return json.loads(text.replace("`", "").replace("JSON", "").replace("json",""))
+    else:
+        return "No text found in the response."
