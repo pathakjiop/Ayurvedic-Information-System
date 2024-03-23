5a0c4a4f9893f36c5ee788208a01e4eaf222d353
Atharv <atharvpathak248@gmail.com>
2024-03-23 10:37:22 +0530

    Fixed Map Error

--- AIS/Back-End/app/__init__.py
+++ AIS/Back-End/app/__init__.py
@@ -4,5 +4,4 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-# Import routes after app is initialized to avoid circular imports
 from app import routes
--- AIS/Front-End/src/components/HerbDetails.js
+++ AIS/Front-End/src/components/HerbDetails.js
@@ -1,29 +1,28 @@
 // HerbDetails.js
-import React from 'react';
-import styles from './HerbDetails.module.css'
+import React from "react";
+import styles from "./HerbDetails.module.css";
 
 const HerbDetails = ({ herbData }) => {
+  const renderSubComponent = (subComponent) => {
+    return (
+      <>
+        {Array.isArray(subComponent) ? (
+          subComponent.map((subPoint, index) => <li key={index}>{subPoint}</li>)
+        ) : (
+          <li>{subComponent}</li>
+        )}
+      </>
+    );
+  };
   return (
     <div className={styles.herbDetails}>
-      <h2>Herb Name: {herbData['Herb Name']}</h2>
+      <h2>Herb Name: {herbData["Herb Name"]}</h2>
       <h3>Benefits:</h3>
-     <ul>
-        {herbData.Benefits.map((benefit, index) => (
-          <li key={index}>{benefit}</li>
-        ))}
-      </ul>
+      <ul>{renderSubComponent(herbData.Benefits)}</ul>
       <h3>Properties:</h3>
-      <ul>
-        {herbData.Properties.map((property, index) => (
-          <li key={index}>{property}</li>
-        ))}
-      </ul>
+      <ul>{renderSubComponent(herbData.Properties)}</ul>
       <h3>Precautions:</h3>
-      <ul>
-        {herbData.Precautions.map((precaution, index) => (
-          <li key={index}>{precaution}</li>
-        ))}
-      </ul>
+      <ul>{renderSubComponent(herbData.Precautions)}</ul>
     </div>
   );
 };
